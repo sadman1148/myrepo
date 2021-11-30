197990543c61204ae5a04de3ba8b5d19ec622e29
sadman1148 <sadman.alam.reshad@g.bracu.ac.bd>
2021-12-01 03:47:28 +0600

    Completed contacts activity Pushed from PC 1st December, 3:47 AM

--- BJITAssignment/app/src/main/java/com/redenvy/bjitassignment/contact.java
+++ BJITAssignment/app/src/main/java/com/redenvy/bjitassignment/contact.java
@@ -1,11 +1,15 @@
 package com.redenvy.bjitassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class contact extends AppCompatActivity {
 
@@ -14,20 +18,40 @@ public class contact extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contact);
     }
+
+    public void run(Intent intent){
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        else{
+            Log.d("ImplicitIntents", "Can't handle this!");
+            Toast.makeText(this, "No Compatible App Found", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     public void mailThisGuy(View view){
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto:" + "sadman.alam@bjitgroup.com"));
         intent.putExtra(Intent.EXTRA_SUBJECT, "Automated Email");
         intent.putExtra(Intent.EXTRA_TEXT, "Hi, this email was sent to you from an app that is under development.");
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
+        intent.putExtra(Intent.EXTRA_CC, new String[]{"sadman.alam.reshad@g.bracu.ac.bd"});
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "bjit.svg");
+        if (!file.exists() && !file.canRead()) {
+            Log.e("ImplicitIntents", "File is not in the downloads folder");
         }
+        intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+        run(intent);
     }
     public void callThisGuy(View view){
         Intent intent = new Intent(Intent.ACTION_DIAL);
         intent.setData(Uri.parse("tel:" + "01994660589"));
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
+        run(intent);
     }
+
+    public void findThisGuy(View view){
+        Uri location = Uri.parse("geo:23.87045238064482, 90.4191694120802?z=18");
+        Intent intent = new Intent(Intent.ACTION_VIEW, location);
+        run(intent);
+    }
+
 }
