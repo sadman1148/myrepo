94c9eaaaae655313f69bc67b5ac39fbe334060d8
sadman1148 <sadman.alam.reshad@g.bracu.ac.bd>
2021-12-15 04:20:19 +0600

    drawer test app 4:21 AM 15th Dec 2021

---
+++ DrawerTestApp/app/src/main/java/com/redenvy/drawertestapp/asef_trans.java
@@ -0,0 +1,14 @@
+package com.redenvy.drawertestapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class asef_trans extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asef_trans);
+    }
+}
--- DrawerTestApp/app/src/main/java/com/redenvy/drawertestapp/transition.java
+++ DrawerTestApp/app/src/main/java/com/redenvy/drawertestapp/transition.java
@@ -13,6 +13,7 @@ import com.redenvy.drawertestapp.databinding.ActivityTransitionBinding;
 public class transition extends AppCompatActivity implements View.OnClickListener {
     private ActivityTransitionBinding binding;
     private ActivityOptions options;
+    private Pair [] p;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,23 +21,34 @@ public class transition extends AppCompatActivity implements View.OnClickListene
         setContentView(binding.getRoot());
         binding.linlay1.setOnClickListener(this);
         binding.linlay2.setOnClickListener(this);
+        binding.linlay3.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view){
         switch (view.getId()){
             case R.id.linlay1:
-                animate(binding.transPp1,"profilePic1");
+                p = new Pair[2];
+                p[0] = new Pair<View,String>(binding.transPp1,"profilePic1");
+                p[1] = new Pair<View,String>(binding.transNameText1,"name1");
+                options = ActivityOptions.makeSceneTransitionAnimation(transition.this, p);
                 startActivity(new Intent(transition.this,sadman_trans.class),options.toBundle());
                 break;
             case R.id.linlay2:
-                animate(binding.transPp2,"profilePic2");
+                p = new Pair[2];
+                p[0] = new Pair<View,String>(binding.transPp2,"profilePic2");
+                p[1] = new Pair<View,String>(binding.transNameText2,"name2");
+                options = ActivityOptions.makeSceneTransitionAnimation(transition.this, p);
                 startActivity(new Intent(transition.this,eftekhar_trans.class),options.toBundle());
                 break;
+            case R.id.linlay3:
+                p = new Pair[2];
+                p[0] = new Pair<View,String>(binding.transPp3,"profilePic3");
+                p[1] = new Pair<View,String>(binding.transNameText3,"name3");
+                options = ActivityOptions.makeSceneTransitionAnimation(transition.this, p);
+                startActivity(new Intent(transition.this,asef_trans.class),options.toBundle());
+            default:
+                break;
         }
-    }
-
-    public void animate(View v, String sharedEleName){
-        options = ActivityOptions.makeSceneTransitionAnimation(transition.this,v,sharedEleName);
     }
 }
