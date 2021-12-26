6b0ddf32d94b849b7280dac55aa2b41ca1bba347
Sadman Alam Reshad <sadman.alam@bjitgroup.com>
2021-12-26 17:37:19 +0600

    pushing from laptop

--- SomeTask/app/src/main/java/com/example/sometask/data/remote/remoteAPI.kt
+++ SomeTask/app/src/main/java/com/example/sometask/data/remote/remoteAPI.kt
@@ -1,8 +1,9 @@
 package com.example.sometask.data.remote
 
+import com.example.sometask.data.apiURL
 import retrofit2.http.GET
 
 interface remoteAPI{
-    @GET("www.ggez.com")
+    @GET(apiURL.NEW_MOVIE_LIST)
     fun movieList()
 }
